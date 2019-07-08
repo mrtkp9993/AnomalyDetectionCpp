c39bfdd0a69466980db02cfe7b81425bb0d517df
Murat Koptur <mkoptur3@gmail.com>
2019-07-08 07:06:40 +0300

    compiler warnings fixed with decltype

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cmath>
 #include <fstream>
 #include <iostream>
@@ -6,9 +7,8 @@
 #include <sstream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
-std::vector<double> parseCsv(const std::string& filename) {
+std::vector<double> parseCsv(const std::string &filename) {
   std::vector<double> data;
   std::fstream file(filename);
   std::string line;
@@ -18,13 +18,13 @@ std::vector<double> parseCsv(const std::string& filename) {
   return data;
 }
 
-double mean(const std::vector<double>& data) {
+double mean(const std::vector<double> &data) {
   double psum = std::accumulate(data.begin(), data.end(), 0.0);
   double pmean = psum / data.size();
   return pmean;
 }
 
-double stddev(const std::vector<double>& data) {
+double stddev(const std::vector<double> &data) {
   double pmean = mean(data);
   double acc = 0.0;
   std::for_each(data.begin(), data.end(),
@@ -33,11 +33,11 @@ double stddev(const std::vector<double>& data) {
   return sstddev;
 }
 
-std::vector<double> detect_anomalies(const std::vector<double>& data) {
+std::vector<double> detect_anomalies(const std::vector<double> &data) {
   std::vector<double> anomalies;
   double data_mean = mean(data);
   double data_stddev = stddev(data);
-  for (int i = 0; i < data.size(); i++) {
+  for (decltype(data.size()) i = 0; i < data.size(); i++) {
     double score = data[i] - data_mean;
     if (std::abs(score) > 3 * data_stddev) {
       anomalies.push_back(data[i]);
@@ -46,10 +46,10 @@ std::vector<double> detect_anomalies(const std::vector<double>& data) {
   return anomalies;
 }
 
-std::vector<double> detect_anomalies_moving(const std::vector<double>& data,
+std::vector<double> detect_anomalies_moving(const std::vector<double> &data,
                                             int window_size) {
   std::vector<double> anomalies;
-  for (int i = window_size - 1; i < data.size(); i++) {
+  for (decltype(data.size()) i = window_size - 1; i < data.size(); i++) {
     std::vector<double> window = std::vector<double>(
         data.begin() + i - window_size + 1, data.begin() + i + 1);
     double moving_mean = mean(window);
@@ -62,20 +62,21 @@ std::vector<double> detect_anomalies_moving(const std::vector<double>& data,
   return anomalies;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   if (argc < 2) {
     std::cerr << "Usage: \n\tAnomalyDetectionCpp filename window_size";
     return 1;
   } else {
     std::vector<double> data = parseCsv(argv[1]);
-    std::vector<double> anomalies_moving = detect_anomalies_moving(data, std::stoi(argv[2]));
+    std::vector<double> anomalies_moving =
+        detect_anomalies_moving(data, std::stoi(argv[2]));
     std::vector<double> anomalies_global = detect_anomalies(data);
     std::cout << "Detected anomalies with moving averages: " << std::endl;
-    for (auto& i : anomalies_moving) {
+    for (auto &i : anomalies_moving) {
       std::cout << i << std::endl;
     }
     std::cout << "Detected anomalies with global average: " << std::endl;
-    for (auto& i : anomalies_global) {
+    for (auto &i : anomalies_global) {
       std::cout << i << std::endl;
     }
   }
